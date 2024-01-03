e9629f852330e12020e8c83218f85ca7594f9055
sammy ocharo <obanyisammy@gmail.com>
2024-01-03 09:41:37 +0300

    first commit 3rd Jan

---
+++ BubbleSort.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    //bubble sort algorithm
+    public static int[] BubbleSortFunc (int[] unsortedArray){
+
+        int temp = 0;
+
+        for(int i=0; i< unsortedArray.length; i++){
+            for(int j=0; j<unsortedArray.length-1; j++){
+                if(unsortedArray[j]>unsortedArray[j+1]){
+                    temp = unsortedArray[j];
+                    unsortedArray[j] = unsortedArray[j+1];
+                    unsortedArray[j+1] = temp;
+                }
+            }
+        }
+
+        return unsortedArray;
+    }
+
+    public  static void main(String[] args){
+
+        int[] numberList = {73,75, 56,32, 65, 52};
+
+        System.out.println(Arrays.toString(BubbleSortFunc(numberList)));
+    }
+}
