e27620f118bc7d4b1e5613a8e4ea5a8fef647804
sammy ocharo <obanyisammy22@gmail.com>
2024-01-05 22:55:50 +0300

    second commit commit 5th Jan

---
+++ Animal.java
@@ -0,0 +1,9 @@
+public class Animal {
+    //Write a Java program to create a class called Animal with a method named move().
+    // Create a subclass called Cheetah that overrides the move() method to run.
+
+    public void move(){
+        System.out.println("The animal is moving");
+    }
+
+}
