258930abac179652be8e74198c9be798243cb97a
sammy ocharo <obanyisammy22@gmail.com>
2024-01-08 23:04:54 +0300

    second commit 8th Jan

---
+++ ArrayOfMultiples.java
@@ -0,0 +1,19 @@
+public class ArrayOfMultiples {
+    //Create a function that takes two numbers as arguments (num, length) and returns an array of multiples of num until the array length reaches length.
+    //
+    //Examples
+    //arrayOfMultiples(7, 5) ➞ [7, 14, 21, 28, 35]
+    //
+    //arrayOfMultiples(12, 10) ➞ [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]
+    //
+    //arrayOfMultiples(17, 6) ➞ [17, 34, 51, 68, 85, 102]
+
+//    public int[] funcIntArray(int num, int length){
+//
+//        int i=0;
+//
+//        while (i <= length){
+//
+//        }
+//    }
+}
